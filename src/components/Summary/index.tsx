51336b888ff8d5b5269d2e20ecba6a14e83c07fb
Filipe Rodrigues <lipe_rsilva@hotmail.com>
2022-02-05 10:35:37 -0300

    ajuste nos arquivos index

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -12,6 +12,7 @@ export function Summary(){
   const summary = transactions.reduce((acc,transaction) => {
     if(transaction.type === 'deposit') {
       acc.deposits += transaction.amount;
+      acc.total += transaction.amount;
     } else {
       acc.withdraws += transaction.amount;
       acc.total -= transaction.amount;
